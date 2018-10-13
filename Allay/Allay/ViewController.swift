ce6407e4336395f8902ca5e6fcac92599d7560fa
Jesse Cook <jessemichaelcook@gmail.com>
2018-10-12 21:40:22 -0700

    Adding the initial build for the iOS app Allay.

---
+++ Allay/Allay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Allay
+//
+//  Created by Jesse Cook on 10/12/18.
+//  Copyright © 2018 Jesse Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
