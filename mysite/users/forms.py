00b3b16566f03ca11e030a3cc099b55bf50c6cb1
nauld <34110472+nauld@users.noreply.github.com>
2018-10-31 22:38:52 -0700

    Add files via upload
    UI changes for user registration. User forms.py changed. STATIC_ROOT added to mysite/settings.py

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -4,8 +4,27 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    #email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        #widgets = {
+         #   'username': forms.TextInput(attrs={'class': 'usernameClass', 'placeholder' : 'username'}),
+         #   'email' : forms.EmailInput(attrs={'class': 'emailClass', 'placeholder' : 'email'}),
+         #   'password1' : forms.PasswordInput(attrs={'class': 'pass1Class', 'placeholder' : 'password'}),
+         #   'password2' : forms.PasswordInput(attrs={'class': 'pass2Class', 'placeholder' : 're-enter password'}),
+        #}
+    
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget = forms.TextInput(attrs={'class': 'usernameClass', 'placeholder' : 'username'})
+        self.fields['username'].label = ''
+        self.fields['email'].widget = forms.EmailInput(attrs={'class': 'emailClass', 'placeholder' : 'email'})
+        self.fields['email'].label = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password1'].label = ''
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'pass1Class', 'placeholder' : 'password',})
+        self.fields['password2'].label = ''
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'pass2Class', 'placeholder' : 'password confirmation',})
