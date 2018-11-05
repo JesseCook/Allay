19e74ca1a3b6db6f9f14dc1a8c301c2c476a2d48
Kasia Sayles <kodingkas@gmail.com>
2018-11-04 21:06:00 -0800

    made it pretty

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -10,21 +10,23 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         #widgets = {
-         #   'username': forms.TextInput(attrs={'class': 'usernameClass', 'placeholder' : 'username'}),
-         #   'email' : forms.EmailInput(attrs={'class': 'emailClass', 'placeholder' : 'email'}),
-         #   'password1' : forms.PasswordInput(attrs={'class': 'pass1Class', 'placeholder' : 'password'}),
-         #   'password2' : forms.PasswordInput(attrs={'class': 'pass2Class', 'placeholder' : 're-enter password'}),
+         #   'username': forms.TextInput(attrs={'class': 'usernameClass text-field', 'placeholder' : 'username'}),
+         #   'email' : forms.EmailInput(attrs={'class': 'emailClass text-field', 'placeholder' : 'email'}),
+         #   'password1' : forms.PasswordInput(attrs={'class': 'pass1Class text-field', 'placeholder' : 'password'}),
+         #   'password2' : forms.PasswordInput(attrs={'class': 'pass2Class text-field', 'placeholder' : 're-enter password'}),
         #}
     
     def __init__(self, *args, **kwargs):
         super(UserRegisterForm, self).__init__(*args, **kwargs)
         
-        self.fields['username'].widget = forms.TextInput(attrs={'class': 'usernameClass', 'placeholder' : 'username'})
+        self.fields['username'].widget = forms.TextInput(attrs={'class': 'usernameClass text-field col-md-12 row', 'placeholder' : 'username'})
+        self.fields['username'].help_text = ''
         self.fields['username'].label = ''
-        self.fields['email'].widget = forms.EmailInput(attrs={'class': 'emailClass', 'placeholder' : 'email'})
+        self.fields['email'].widget = forms.EmailInput(attrs={'class': 'emailClass text-field col-md-12 row', 'placeholder' : 'email'})
         self.fields['email'].label = ''
         self.fields['password1'].help_text = ''
         self.fields['password1'].label = ''
-        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'pass1Class', 'placeholder' : 'password',})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'pass1Class text-field col-md-12 row', 'placeholder' : 'password',})
         self.fields['password2'].label = ''
-        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'pass2Class', 'placeholder' : 'password confirmation',})
+        self.fields['password2'].help_text = ''
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'pass2Class text-field col-md-12 row', 'placeholder' : 'confirm password',})
