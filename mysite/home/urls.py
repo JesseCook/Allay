c6b2ea8d0f721d182b538ca36049b9fa883a41c8
nauld <34110472+nauld@users.noreply.github.com>
2018-11-13 00:39:13 -0800

    Add files via upload
    added Anxiety and Depression overview pages

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
         path('home/', views.home, name='home-home'),
         path('about/', views.about, name='home-about'),
         path('feeling-submission/', views.feeling, name='home-feeling-submission'),
+        path('anxiety-overview/', views.anxietyOverview, name='home-anxiety-overview'),
+        path('depression-overview/', views.depressionOverview, name='home-depression-overview'),
     ]
