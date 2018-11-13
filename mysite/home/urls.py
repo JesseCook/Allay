51c9e5180599dba5edd3dd09409549e520cc49c2
Jesse Cook <jessemichaelcook@gmail.com>
2018-11-13 10:11:35 -0800

    I got the login working properly, with the correct formatting.

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('home/', views.home, name='home-home'),
+        path('', views.home, name='home'),
         path('about/', views.about, name='home-about'),
         path('feeling-submission/', views.feeling, name='home-feeling-submission'),
     ]
