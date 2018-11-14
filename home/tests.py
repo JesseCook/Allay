d6c212a7db6b3acc5a25cba78b01a09ecd2aaee5
Kiran <justlookabove22@gmail.com>
2018-11-13 16:40:06 -0800

    buttons for Anxiety and Depression

---
+++ home/tests.py
@@ -0,0 +1,319 @@
+from django.test import TestCase
+from home.models import *
+from home.forms import *
+from django.contrib.auth.models import User
+from django.test import Client
+'''
+# Create your tests here.
+class SymptomAverageTestCase(TestCase):
+
+    # Sets up our Symptom objects for testing.
+    def setUp(self):
+
+        print("\n----------------------------------------------------------------------")
+
+        self.user = User.objects.create_user(username='test',password='1234')
+
+        Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety,average_rating=10)
+
+        Symptom.objects.create(user=self.user, symptom=Symptom.Depression,average_rating=20)
+
+    # Checks to make sure we can get the average rating for each symptom.
+    def test_Symptom_average_tests(self):
+
+        print("Tests For Getting symptom.average From Symtpom Objects:\n")
+
+        self.anxiety_average = Symptom.objects.get(symptom='Anxiety')
+
+        self.depression_average = Symptom.objects.get(symptom='Depression')
+
+        print('anxiety_average.average_rating:',self.anxiety_average.symptom+":", self.anxiety_average.average_rating)
+
+        print('depression_average.average_rating:', self.depression_average.symptom+":", self.depression_average.average_rating)
+
+
+    # Checks to make sure we can get the user, and user_id, for each symptom.
+    def test_Symptom_user_tests(self):
+        print("Tests For Working With symptom.user From Symptom Objects:\n")
+
+        self.anxiety_average = Symptom.objects.get(symptom='Anxiety')
+
+        self.depression_average = Symptom.objects.get(symptom='Depression')
+
+        print('anxiety_average.user:', self.anxiety_average.user)
+
+        print('depression_average.user:', self.depression_average.user)
+
+        print('anxiety_average.user_id:', self.anxiety_average.user_id)
+
+        print('depression_average.user_id:', self.depression_average.user_id)
+
+        print("----------------------------------------------------------------------")
+
+class DayTestCase(TestCase):
+
+    # Sets up out Day objects for testing.
+    def setUp(self):
+
+        self.user = User.objects.create_user(username='test', password='1234')
+
+        self.symptomAnxiety = Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety,average_rating=30)
+
+        self.symptomDepression = Symptom.objects.create(user=self.user, symptom=Symptom.Depression,average_rating=40)
+
+        Day.objects.create(symptom=self.symptomAnxiety, rating=50)
+
+        Day.objects.create(symptom=self.symptomDepression,rating=60)
+
+
+    # Checks to make sure we can get the rating from each Day.
+    def test_Day_rating_tests(self):
+
+        self.test_anxiety = Day.objects.get(symptom=self.symptomAnxiety.id)
+
+        self.test_depression = Day.objects.get(symptom=self.symptomDepression.id)
+
+        print("--------------------------------------------------------------------")
+
+        print("Tests For Getting day.rating From Day Objects\n")
+
+        print('test_anxiety.rating: ',self.test_anxiety.rating)
+
+        print('test_depression.rating: ',self.test_depression.rating)
+
+        print("--------------------------------------------------------------------")
+
+    # Checks to make sure that we can get the symptom, and symptom_id, that each Day is linked to.
+    def test_Day_symptom_tests(self):
+
+        self.test_anxiety = Day.objects.get(symptom=self.symptomAnxiety.id)
+
+        self.test_depression = Day.objects.get(symptom=self.symptomDepression.id)
+
+        print("Tests For Working With day.symptom For A Day Object:\n")
+
+        print('test_anxiety.symptom: ', self.test_anxiety.symptom)
+
+        print('test_depression.symptom: ',  self.test_depression.symptom)
+
+        print('test_anxiety.symptom_id: ', self.test_anxiety.symptom_id)
+
+        print('test_depression.symptom_id: ', self.test_depression.symptom_id)
+
+        print("---------------------------------------------------------------------")
+
+
+    # Checks to mae sure that we can get the user, and user_id, that each Day is linked to.
+    def test_Day_user_tests(self):
+
+        self.test_anxiety = Day.objects.get(symptom=self.symptomAnxiety.id)
+
+        self.test_depression = Day.objects.get(symptom=self.symptomDepression.id)
+
+        print("---------------------------------------------------------------------")
+
+        print("Test For Getting The user.id And user From A User Linked To A Day :\n")
+
+        print('The user.id of test_anxiety: ', self.test_anxiety.symptom.user.id)
+
+        print('The user.id of test_depression: ', self.test_depression.symptom.user.id)
+
+        print('The user of test_anxiety: ', self.test_anxiety.symptom.user)
+
+        print('The user of test_depression: ', self.test_depression.symptom.user)
+
+class DayLogTestCase(TestCase):
+
+    # Sets up the DayLog objects for testing.
+    def setUp(self):
+
+        print("\n----------------------------------------------------------------------")
+
+        self.user = User.objects.create_user(username='test', password='1234')
+
+        self.symptomAnxiety = Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety, average_rating=30)
+
+        self.symptomDepression = Symptom.objects.create(user=self.user, symptom=Symptom.Depression, average_rating=40)
+
+        self.dayAnxiety = Day.objects.create(symptom=self.symptomAnxiety, rating=50)
+
+        self.dayDepression = Day.objects.create(symptom=self.symptomDepression, rating=60)
+
+        DayLog.objects.create(day=self.dayAnxiety,rating=self.dayAnxiety.rating,log='This is a test for anxiety.')
+
+        DayLog.objects.create(day=self.dayDepression,rating=self.dayDepression.rating,log='This is a test for depression.')
+
+    # Checks to make sure that we can get the rating for each day.
+    def test_DayLog_rating_tests(self):
+
+        self.test_anxiety = DayLog.objects.get(day=self.dayAnxiety.id)
+
+        self.test_depression = DayLog.objects.get(day=self.dayDepression.id)
+
+        print("----------------------------------------------------------------------")
+
+        print("Tests For Getting rating From A DayLog:\n")
+
+        print("test_anxiety.rating: ",self.test_anxiety.rating)
+
+        print("test_depression.rating: ",self.test_depression.rating)
+
+        print("----------------------------------------------------------------------")
+
+    # Checks to make sure that we can get the day, and day_id, that each DayLog is linked to.
+    def test_DayLog_day_tests(self):
+        self.test_anxiety = DayLog.objects.get(day=self.dayAnxiety.id)
+
+        self.test_depression = DayLog.objects.get(day=self.dayDepression.id)
+
+        print("Tests For Getting day_id And day From A DayLog:\n")
+
+        print("test_anxiety.day_id: ", self.test_anxiety.day_id)
+
+        print("test_depression.day_id: ", self.test_depression.day_id)
+
+        print("test_anxiety.day.day: ",self.test_anxiety.day.day)
+
+        print("test_depression.day.day: ",self.test_depression.day.day)
+
+        print("----------------------------------------------------------------------")
+
+
+    # Checks to make sure that we can get the symptom, and symptom_id, that each DayLog is linked to.
+    def test_DayLog_symptom_tests(self):
+
+        self.test_anxiety = DayLog.objects.get(day=self.dayAnxiety.id)
+
+        self.test_depression = DayLog.objects.get(day=self.dayDepression.id)
+
+
+        print("Tests For Getting symptom_id And symptom From A DayLog:\n")
+
+        print("test_anxiety.day.symptom_id: ", self.test_anxiety.day.symptom_id)
+
+        print("test_depression.day.symptom_id: ", self.test_depression.day.symptom_id)
+
+        print("test_anxiety.day.symptom: ", self.test_anxiety.day.symptom)
+
+        print("test_depression.day.symptom: ", self.test_depression.day.symptom)
+
+        print("----------------------------------------------------------------------")
+
+    # Checks to make sure that we can get the user, and user_id, that each DayLog is linked to.
+    def test_DayLog_user_tests(self):
+
+        self.test_anxiety = DayLog.objects.get(day=self.dayAnxiety.id)
+
+        self.test_depression = DayLog.objects.get(day=self.dayDepression.id)
+
+        print("Tests For Getting user_id And user From A DayLog:\n")
+
+        print("test_anxiety.day.symptom.user_id: ", self.test_anxiety.day.symptom.user_id)
+
+        print("test_depression.day.symptom.user_id: ", self.test_depression.day.symptom.user_id)
+
+        print("test_anxiety.day.symptom.user: ", self.test_anxiety.day.symptom.user)
+
+        print("test_depression.day.symptom.user: ", self.test_depression.day.symptom.user)
+
+
+    # Checks to make sure that can get the log for each DayLog.
+    def test_DayLog_log_tests(self):
+        self.test_anxiety = DayLog.objects.get(day=self.dayAnxiety.id)
+
+        self.test_depression = DayLog.objects.get(day=self.dayDepression.id)
+
+        print("Tests For Getting log From A DayLog:\n")
+
+        print("test_anxiety.log: ", self.test_anxiety.log)
+
+        print("test_depression.log: ", self.test_depression.log)
+
+class SymptomFormTest(TestCase):
+
+    def setUp(self):
+       self.user = User.objects.create_user(username='test', password='1234')
+
+       self.symptomAnxiety = Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety, average_rating=8.0)
+
+       self.symptomDepression = Symptom.objects.create(user=self.user, symptom=Symptom.Depression, average_rating=7.6)
+
+    def test_SymptomForm_tests(self):
+        form_data = {'symptom':Symptom.Anxiety,'average_rating': 10}
+
+        test_form = SymptomForm(data=form_data)
+        self.assertTrue(test_form.is_valid())
+
+        anxiety_symptom_form = SymptomForm({'symptom': self.symptomAnxiety.symptom,'average_rating':self.symptomAnxiety.average_rating}, instance= self.symptomAnxiety)
+
+        print(anxiety_symptom_form.is_bound)
+
+        self.assertTrue(anxiety_symptom_form.is_valid())
+
+        #for value in anxiety_symptom_form.fields.values(): print(value)
+
+        print(anxiety_symptom_form.cleaned_data)
+
+class DayFormTest(TestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create_user(username='test', password='1234')
+
+        self.symptomAnxiety = Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety,average_rating=30)
+
+        self.symptomDepression = Symptom.objects.create(user=self.user, symptom=Symptom.Depression,average_rating=40)
+
+        self.dayAnxiety = Day.objects.create(symptom=self.symptomAnxiety, rating=5.0)
+
+        self.dayDepression = Day.objects.create(symptom=self.symptomDepression,rating=6.0)
+
+    def test_DayForm_tests(self):
+
+        anxiety_day_form = DayForm({'rating':self.dayAnxiety.rating},instance=self.dayAnxiety)
+
+        depression_day_from = DayForm({'rating':self.dayDepression.rating},instance=self.dayDepression)
+
+        self.assertTrue(anxiety_day_form.is_valid())
+
+        self.assertTrue(depression_day_from.is_valid())
+
+        print(anxiety_day_form.cleaned_data)
+
+        print(depression_day_from.cleaned_data)
+
+'''
+class DayLogFormTest(TestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create_user(username='test', password='1234')
+
+        self.symptomAnxiety = Symptom.objects.create(user=self.user, symptom=Symptom.Anxiety, average_rating=30)
+
+        self.symptomDepression = Symptom.objects.create(user=self.user, symptom=Symptom.Depression, average_rating=40)
+
+        self.dayAnxiety = Day.objects.create(symptom=self.symptomAnxiety, rating=5.0)
+
+        self.dayDepression = Day.objects.create(symptom=self.symptomDepression, rating=6.0)
+
+        self.dayLogAxniety = DayLog.objects.create(day=self.dayAnxiety, log='This is a test for anxiety.')
+
+        self.dayLogDepression = DayLog.objects.create(day=self.dayDepression, log='This is a test for depression.')
+
+    def test_DayLogForm_tests(self):
+
+        anxiety_daylog_form = DayLogForm({'log':self.dayLogAxniety.log},instance= self.dayLogAxniety)
+
+        depression_daylog_form = DayLogForm({'log':self.dayLogDepression.log},instance= self.dayLogDepression)
+
+        self.assertTrue(anxiety_daylog_form.is_valid())
+
+        self.assertTrue(depression_daylog_form.is_valid())
+
+        print(anxiety_daylog_form.cleaned_data)
+
+        print(depression_daylog_form.cleaned_data)
+
+
+        
---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.home, name='home'),
+        path('about/', views.about, name='home-about'),
+        path('anxiety-overview/feeling-submission/', views.feeling, name='home-anxiety-feeling-submission'),
+        path('depression-overview/feeling-submission/', views.feeling, name='home-depression-feeling-submission'),
+        path('anxiety-overview/', views.anxietyOverview, name='home-anxiety-overview'),
+        path('depression-overview/', views.depressionOverview, name='home-depression-overview'),
+    ]
