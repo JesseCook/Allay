c3389a32feffeaa7f6d18fff62e36298ba5e7064
thisoneguy365 <justlookabove22@gmail.com>
2018-11-17 23:45:27 -0800

    Buttons Round 2

--- mysite/mysite/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-        path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
- ]
