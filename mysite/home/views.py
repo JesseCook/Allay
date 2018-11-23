f6ee08e9f8b165b26270b160ed001143f0ee1f5e
nauld <34110472+nauld@users.noreply.github.com>
2018-11-23 14:24:28 -0800

    Add files via upload

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from home.models import *
 
 
 # Want it so when user enters website, it immediately goes
@@ -22,3 +24,8 @@ def anxietyOverview(request):
 
 def depressionOverview(request):
     return render(request, 'home/depression-overview.html')
+
+class dayList(generic.ListView):
+    model = Day
+    context_object_name = 'day-list' #custom name for day list as a template variable
+    template_name = 'home/day-list.html' #custom template name/location
